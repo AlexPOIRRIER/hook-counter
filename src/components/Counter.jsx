b0fd175484fe0b4e298a61cc23d4b71031ceada7
Alexandre POIRRIER <alex.poirrier@gmail.com>
2020-10-24 13:47:59 +0200

    SET the starting value of the count

---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  let [count, setCount] = useState(0);
+
+  const handleChange = (event)=> 
+    count = event.target.value;
+
+  return (
+    <div>
+        <input 
+        onChange={handleChange}
+        />
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(+count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
